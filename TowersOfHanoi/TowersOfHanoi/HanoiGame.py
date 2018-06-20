6295b03602c2c81f6c335441b1999cd3e13b499a
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-06-20 08:42:36 +0200

    Add self Keyword, Stack

--- TowersOfHanoi/TowersOfHanoi/HanoiGame.py
+++ TowersOfHanoi/TowersOfHanoi/HanoiGame.py
@@ -9,20 +9,26 @@ class HanoiGame:
             raise ValueError("The number of disks must be given")
         if numberOfDisks < 2 or numberOfDisks > 10:
             raise ValueError("The number of disks has to be between 2 and 10")
-        for i in range(3,-1,-1):
-            self._firstStack
+        self._initialize_Game_Field(numberOfDisks)
+
+    def _initialize_Game_Field(self, numberOfDisks):
+         for i in range(3,-1,-1):
+            self._firstStack.append(i)
         
     def turn(self, start, end):
         startStack = self._get_stack(start)
         endStack = self._get_stack(end)
         self._check_turn(start, end)
-        print(start + end)
+        
     
     def _check_turn(self, start, end):
         if start is end:
            raise ValueError("The start stack could not be the end stack")
 
         startStack = self._get_stack(start)
+        endStack = self._get_stack(end)
+
+       
 
     def _get_stack(self, input):
        if input is 1:
